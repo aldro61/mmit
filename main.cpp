584c540b70af538db9056003a80086a53278144c
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-03-10 10:13:18 -0500

    Randomly adding a lot of points

--- main.cpp
+++ main.cpp
@@ -14,6 +14,14 @@ int main() {
     test.insert_point(0.0, true);
     test.insert_point(0.5, false);
 
+    for(int i = 0; i < 50000; i++){
+        bool is_upper_bound = ((double) rand() / (RAND_MAX)) < 0.5 ? true : false;
+        int multiplier =  ((double) rand() / (RAND_MAX)) < 0.5 ? -1 : 1;
+        double y = rand() * multiplier;
+        test.insert_point(y, is_upper_bound);
+        cout << "Minimum: " << test.get_minimum_value() << endl;
+    }
+    cout << "Finished" << endl;
     return 0;
 }
 
