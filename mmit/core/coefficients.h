40abe9ae287c92a29fc3c2eabb443752e585c87c
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-03-15 11:12:18 -0400

    Added copyright and license notice

--- mmit/core/coefficients.h
+++ mmit/core/coefficients.h
@@ -1,6 +1,20 @@
+/*
+MMIT: Max Margin Interval Trees
+Copyright (C) 2017 Toby Dylan Hocking, Alexandre Drouin
+This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #ifndef MMIT_COEFFICIENTS_H
 #define MMIT_COEFFICIENTS_H
-
 
 class Coefficients{
 public:
