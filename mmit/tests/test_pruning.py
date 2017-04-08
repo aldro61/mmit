7a7bdbe7b2821579469f2c9747760855893dcb87
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-04-08 00:53:40 -0400

    Added a test for a multistep pruning

--- mmit/tests/test_pruning.py
+++ mmit/tests/test_pruning.py
@@ -17,6 +17,7 @@ from __future__ import print_function
 import numpy as np
 import sys
 
+from sklearn.utils.validation import _NotFittedError
 from unittest import TestCase
 
 from ..pruning import min_cost_complexity_pruning
@@ -28,7 +29,14 @@ def eprint(*args, **kwargs):
 
 
 class PruningTests(TestCase):
-    def test_1(self):
+    def test_requires_fitted_estimator(self):
+        """
+        Test that requires fitted estimator
+        """
+        estimator = MaxMarginIntervalTree()
+        self.assertRaises(_NotFittedError, min_cost_complexity_pruning, estimator)
+
+    def test_initial_pruning(self):
         """
         Test initial pruning (Tmax -> T1)
         """
@@ -38,8 +46,8 @@ class PruningTests(TestCase):
         root = RegressionTreeNode(0, [], rule="rule1", cost_value=10.)
 
         # Add a split that does not decrease the objective (should be pruned in the initial pruning)
-        root.left_child = RegressionTreeNode(1, [], rule="rule2", cost_value=5, parent=root)
-        root.right_child = RegressionTreeNode(1, [], rule="rule3", cost_value=5, parent=root)
+        root.left_child = RegressionTreeNode(1, [], cost_value=5, parent=root)
+        root.right_child = RegressionTreeNode(1, [], cost_value=5, parent=root)
 
         estimator.tree_ = root
 
@@ -49,3 +57,30 @@ class PruningTests(TestCase):
         assert pruned_trees[0].tree_.is_leaf and pruned_trees[0].tree_.is_root
         np.testing.assert_almost_equal(actual=alphas, desired=[0.])
         np.testing.assert_almost_equal(actual=pruned_trees[0].tree_.cost_value, desired=10.)
+
+    def test_pruning_1(self):
+        """
+        Test multistep pruning #1
+        """
+        estimator = MaxMarginIntervalTree()
+        estimator.rule_importances_ = []
+
+        root = RegressionTreeNode(0, [], rule="root", cost_value=10.)
+
+        root_l = root.left_child = RegressionTreeNode(1, [], rule="root_l", cost_value=5, parent=root)
+        root_l_l = root_l.left_child = RegressionTreeNode(2, [], cost_value=0, parent=root_l)
+        root_l_r = root_l.right_child = RegressionTreeNode(2, [], cost_value=0, parent=root_l)
+
+        root_r = root.right_child = RegressionTreeNode(1, [], rule="root_r", cost_value=3, parent=root)
+        root_r_l = root_r.left_child = RegressionTreeNode(2, [], cost_value=0, parent=root_r)
+        root_r_r = root_r.right_child = RegressionTreeNode(2, [], cost_value=1, parent=root_r)
+
+        estimator.tree_ = root
+        alphas, pruned_trees = min_cost_complexity_pruning(estimator)
+        expected_rules = [["root", "root_l", "root_r"],
+                          ["root", "root_l"],
+                          []]
+        expected_alphas = [0., 2., 3.5]
+        for i, pt in enumerate(pruned_trees):
+            np.testing.assert_equal(actual=sorted(expected_rules[i]), desired=sorted(pt.tree_.rules))
+        np.testing.assert_equal(actual=alphas, desired=expected_alphas)
