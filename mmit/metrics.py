846d09fd99048e0809ceb4ecd255fbb15bfa529b
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-05-30 16:01:09 -0400

    Code review: added validation of input arrays

--- mmit/metrics.py
+++ mmit/metrics.py
@@ -21,7 +21,9 @@ def zero_one_loss(y_true, y_pred):
     Proportion of the predicted values that are not in their target interval
 
     """
-    return 1.0 * sum(0 if (t[0] <= p <= t[1]) else 1 for p, t in zip(y_pred, y_true)) / len(y_pred)
+    if len(y_pred) != len(y_true):
+        raise ValueError("The number of predictions must match the number of target interval values.")
+    return float(1.0 * sum(0 if (t[0] <= p <= t[1]) else 1 for p, t in zip(y_pred, y_true)) / len(y_pred))
 
 
 def mean_squared_error(y_true, y_pred):
@@ -29,4 +31,6 @@ def mean_squared_error(y_true, y_pred):
     The mean squared distance to the interval bounds
 
     """
-    return 1.0 * sum((p - t[0])**2 if p < t[0] else ((p - t[1])**2 if t[1] < p else 0.0) for p, t in zip(y_pred, y_true)) / len(y_pred)
+    if len(y_pred) != len(y_true):
+        raise ValueError("The number of predictions must match the number of target interval values.")
+    return float(1.0 * sum((p - t[0])**2 if p < t[0] else ((p - t[1])**2 if t[1] < p else 0.0) for p, t in zip(y_pred, y_true)) / len(y_pred))
