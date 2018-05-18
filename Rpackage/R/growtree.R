09f7e06b5054ef7317b853e934af2102c0fd3e14
parismita <parismita@iitg.ernet.in>
2018-05-19 01:26:22 +0530

    Update growtree.R

--- Rpackage/R/growtree.R
+++ Rpackage/R/growtree.R
@@ -11,18 +11,18 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
   
   kidids <- kidids_split(sp, data = feature.mat) #list of right left kids
   kids <- vector(mode = "list", length = max(kidids, na.rm = TRUE)) #null why
-  #I want a list of elems in left and right********
   
   if(length(kids)<=1) return(partynode(id = id))
   
   for (kidid in 1:length(kids)) {
     if (kidid > 1) {
-      myid <- max(nodeids(kids[[kidid - 1]]))
+      myid <- max(nodeids(kids[[kidid - 1]])) #null for id=2
     } else {
-      myid <- id
+      myid <- id  #id = 1L
     }
     depth <- depth+1
     kids[[kidid]] <- growtree(target.mat, feature.mat, depth = depth, maxdepth = maxdepth, margin = margin, id = as.integer(myid + 1))
+    #If I give target and feature as response and data, next node splits on same as its not subtree
   }
   return(partynode(id = as.integer(id), split = sp, kids = kids,info = list(p.value = min(info_split(sp)$p.value, na.rm = TRUE))))
 })
