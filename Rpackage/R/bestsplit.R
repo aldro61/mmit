ea059cac371d18116fb40b5670c1f971afaf6879
parismita <parismita@iitg.ernet.in>
2018-05-19 00:56:35 +0530

    Update bestsplit.R

--- Rpackage/R/bestsplit.R
+++ Rpackage/R/bestsplit.R
@@ -9,20 +9,25 @@ bestsplit <- structure(function
   best_split$varid <- NULL
   best_split$row <- NULL
 
-  
+  #loop for every feature
   for (index in 2:(length(feature.mat[1,]))){
     feat = feature.mat[, index]
     
-    #unique and sorted
-    sorted <- order(feat)[!duplicated(feat)] 
+    #sorted
+    sorted <- order(feat)
     z<- feat
     feat <- z[sorted]
-    rev_feat <-z[rev(sorted)]     #reverse order
     z<- target.mat
     tar <- z[sorted,]
-    rev_tar <- z[rev(sorted),]     #reverse order
+    rev_tar <- z[rev(sorted),]   #reverse order
     
-    if(length(sorted)==1){
+    #1st and last index of duplicate elem
+    first_idx <- order(feat)[!duplicated(feat)]
+    last_idx <- c(order(feat)[!duplicated(feat)]-1,length(feat))
+    last_idx <- last_idx[-1]
+    
+    #if no unique elements
+    if(length(last_idx)==0){
       next
     }
     
@@ -31,12 +36,22 @@ bestsplit <- structure(function
     rightpred <- compute_optimal_costs(rev_tar,margin)
     
     #unique and removing cases where all examples are in one leaf
-    leftpred <- leftpred[sorted,]
-    rightpred <- rightpred[sorted,]
+    leftpred <- leftpred[last_idx,]
+    leftpred <- leftpred[-length(leftpred[,1]),]
+    rightpred$moves <- rev(rightpred[,1])
+    rightpred$pred <- rev(rightpred[,2])
+    rightpred$cost <- rev(rightpred[,3])
+    rightpred <- rightpred[first_idx,]
+    rightpred <- rightpred[-1,]
     
-    #removing NA cases
+    #removing NA cases and summing both orders
     split_cost <- na.omit(leftpred$cost+rightpred$cost)
-
+    
+    #if no split possible
+    if(length(split_cost)==0){
+      next
+    }
+    
     if(min(split_cost)<best_split$cost){
       best_split$cost <- min(split_cost)
       best_split$br <- feat[which.min(split_cost)]
