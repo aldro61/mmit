e90a88c55d5db13eabd7d405151839a507c42ec8
parismita <parismita@iitg.ernet.in>
2018-05-23 07:58:12 +0530

    Update growtree.R

--- Rpackage/R/growtree.R
+++ Rpackage/R/growtree.R
@@ -1,7 +1,8 @@
-growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,margin=0.0,loss="hinge",id = 1L, min_sample = 1) {
+growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,margin=0.0,loss="hinge",id = 1L, min_sample = 1,pred=NULL, side = NULL) {
 
   #if depth exceeds we stop the further growth of tree
   if (depth > maxdepth) return(partynode(id = id))
+  
 
   #if sample per node is less than minimum, we stop tree growth
   if(length(target.mat[,1])<=min_sample) return(partynode(id = id))
@@ -9,10 +10,13 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
   #split the tree at the node.
   #print(length(feature.mat[,2]))
   sp <- bestsplit(target.mat,feature.mat,margin,loss)
-  sp <- partysplit(varid = sp$varid,br = sp$br)
+  splt <- sp
 
   #if no split, we stop
   if (is.null(sp)) return(partynode(id = id))
+
+  #partysplit object
+  sp <- partysplit(varid = sp$varid,breaks = sp$br)
 
   #the kids left and right defined
   kid<-NULL
@@ -24,9 +28,13 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
 
   #splitting into kids
   kidids <- kidids_split(sp, data = feature.mat) #list of right left kids
-  #print(kidids)
+  print(length(kidids))
 
-  #kid <- kidids_node()
+  #creating partynode list of kids
+  kids <- vector(mode = "list", length = max(kidids, na.rm = TRUE))
+  
+  #if no left right kids, stop tree growth
+  if(length(kids)<=1) return(partynode(id = id))
 
   #creating list of data into
   for(i in 1:length(feature.mat[,2])){
@@ -40,12 +48,6 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
     }
   }
 
-  #creating partynode list of kids
-  kids <- vector(mode = "list", length = max(kidids, na.rm = TRUE))
-
-  #if no left right kids, stop tree growth
-  if(length(kids)<=1) return(partynode(id = id))
-
 
   for (kidid in 1:length(kids)) {
     if (kidid > 1) {
@@ -54,9 +56,9 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
       myid <- id
     }
     depth <- depth+1
-    #print(length(tar[[kidid]][,2]))
-    kids[[kidid]] <- growtree(tar[[kidid]], kid[[kidid]], depth = depth, maxdepth = maxdepth, margin = margin, loss = loss, id = as.integer(myid + 1),min_sample = min_sample)
+    kids[[kidid]] <- growtree(tar[[kidid]], kid[[kidid]], pred=splt, side = 4+kidid, depth = depth, maxdepth = maxdepth, margin = margin, loss = loss, id = as.integer(myid + 1),min_sample = min_sample)
   }
+  
   return(partynode(id = as.integer(id), split = sp, kids = kids))
 })
 
