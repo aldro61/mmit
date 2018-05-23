0b7d2b7e7fb8340b1048c8026e9eed2bac4b1886
parismita <parismita@iitg.ernet.in>
2018-05-23 07:57:08 +0530

    Update mmit.R

--- Rpackage/R/mmit.R
+++ Rpackage/R/mmit.R
@@ -8,12 +8,19 @@ mmit <- structure(function(formula, target.mat, feature.mat, weights = NULL,dept
   node<- growtree(target.mat, feature.mat, maxdepth = maxdepth, margin = margin)
   
   ## compute terminal node number for each observation
-  fitted <- fitted_node(node, data = feature.mat)
-  
-  tree <- party(node, data = feature.mat,
-               fitted = data.frame("(fitted)" = fitted,
-                                   "(response)" = feature.mat[[response]]))
-               #terms = terms(formula))
+  tree <- party(node, data = feature.mat, fitted = data.frame("(fitted)" = fitted_node(node, data = feature.mat),
+                                                          "(response)" = model.response(feature.mat),
+                                                          check.names = FALSE), terms = terms(feature.mat))
+  tree <- as.constparty(tree)
 
   return(tree)
 })
+
+
+#library(survival)
+#data(neuroblastomaProcessed, package="penaltyLearning")
+#survTarget <- with(neuroblastomaProcessed, {Surv(target.mat[, 1], target.mat[,2], type="interval2")})
+#feature.mat <- data.frame(log.penalty=survTarget, neuroblastomaProcessed$feature.mat)[1:45,]
+#target.mat <- neuroblastomaProcessed$target.mat[1:45,]
+#tree <- mmit(log.penalty~.,target.mat, feature.mat, maxdepth = 2,margin = 1.0)
+#plot(tree)
