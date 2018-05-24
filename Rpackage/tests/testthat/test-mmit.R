7c49eb8ef405889f8535215630d5c32857693237
parismita <parismita@iitg.ernet.in>
2018-05-24 11:55:43 +0530

    Update test-mmit.R

--- Rpackage/tests/testthat/test-mmit.R
+++ Rpackage/tests/testthat/test-mmit.R
@@ -3,17 +3,17 @@ context("trivial")
 library(mmit)
 
 target.mat <- rbind(
-  c(0,1),c(0,1),c(0,1),
-  c(2,3),c(2,3),c(2,3))
+  c(0,1), c(0,1), c(0,1),
+  c(2,3), c(2,3), c(2,3))
 
 feature.mat <- rbind(
-  c(1,0,0),c(1,1,0),c(1,2,0),
-  c(1,3,0),c(1,4,0),c(1,5,0))
-colnames(feature.mat) <- c("a","b","c")
+  c(1,0,0), c(1,1,0), c(1,2,0),
+  c(1,3,0), c(1,4,0), c(1,5,0))
+colnames(feature.mat) <- c("a", "b", "c")
 feature.mat <- data.frame(feature.mat)
 
 #linear hinge, margin = 0, test best split
-out<-bestsplit(target.mat,feature.mat,margin=0.0,loss="hinge")
+out<-bestsplit(target.mat, feature.mat, margin=0.0, loss="hinge")
 test_that("margin=0.0 yields cost 0", {
   expect_equal(out$leftpred, 0.5)
   expect_equal(out$rightpred, 2.5)
@@ -24,7 +24,7 @@ test_that("margin=0.0 yields cost 0", {
 })
 
 #square hinge, margin = 0, test best split
-out<-bestsplit(target.mat,feature.mat,margin=0.0,loss="square")
+out<-bestsplit(target.mat, feature.mat, margin=0.0, loss="square")
 test_that("margin=0.0 yields cost 0", {
   expect_equal(out$leftpred, 0.5)
   expect_equal(out$rightpred, 2.5)
@@ -35,7 +35,7 @@ test_that("margin=0.0 yields cost 0", {
 })
 
 #linear hinge, margin = 0, test mmit
-out<-predict(mmit(a~.,target.mat,feature.mat))
+out<-predict(mmit(a~., target.mat, feature.mat))
 test_that("margin=0.0 yields cost 0", {
   expect_equal(out[[1]], 0.5)
   expect_equal(out[[2]], 0.5)
@@ -46,7 +46,7 @@ test_that("margin=0.0 yields cost 0", {
 })
 
 #square hinge, margin = 0, test mmit
-out<-predict(mmit(a~.,target.mat,feature.mat,loss="square"))
+out<-predict(mmit(a~., target.mat, feature.mat, loss="square"))
 test_that("margin=0.0 yields cost 0", {
   expect_equal(out[[1]], 0.5)
   expect_equal(out[[2]], 0.5)
