937a69ddac06753e3d5cd9eb2d7588f0c2099f3c
parismita <parismita@iitg.ernet.in>
2018-05-24 20:46:37 +0530

    Update test-mmit.R
    the error in testthat is when I am not explicitly compiling the mmit, growtree and bestsplit, 
    If those files are sourced explicitly, the error vanishes

--- Rpackage/tests/testthat/test-mmit.R
+++ Rpackage/tests/testthat/test-mmit.R
@@ -10,8 +10,11 @@ feature.mat <- rbind(
   c(1,0,0), c(1,1,0), c(1,2,0),
   c(1,3,0), c(1,4,0), c(1,5,0))
 
+colnames(feature.mat) <- c("a", "b", "c")
+feature.mat <- data.frame(feature.mat)
+
 #linear hinge, margin = 0, test best split
-out<-bestsplit(target.mat, feature.mat, margin=0.0, loss="hinge")
+out <- bestsplit(target.mat, feature.mat, margin=0.0, loss="hinge")
 test_that("margin=0.0 yields cost 0", {
   expect_equal(out$leftpred, 0.5)
   expect_equal(out$rightpred, 2.5)
@@ -32,27 +35,31 @@ test_that("margin=0.0 yields cost 0", {
   expect_equal(out$row, 3)
 })
 
-colnames(feature.mat) <- c("a", "b", "c")
-feature.mat <- data.frame(feature.mat)
+survTarget <- with(feature.mat, {Surv(target.mat[, 1], target.mat[,2], type="interval2")})
+feature.mat <- data.frame(d=survTarget, feature.mat)
 
 #linear hinge, margin = 0, test mmit
-out<-predict(mmit(a~., target.mat, feature.mat))
+out <- mmit(d~., target.mat, feature.mat)
+p <- predict(out)
 test_that("margin=0.0 yields cost 0", {
-  expect_equal(out[[1]], 0.5)
-  expect_equal(out[[2]], 0.5)
-  expect_equal(out[[3]], NA)   #why it should be 0.5
-  expect_equal(out[[4]], 2.5)
-  expect_equal(out[[5]], 2.5)
-  expect_equal(out[[6]], NA)
+  expect_equal(nodeapply(out, ids = p[[1]], info_node)[[1]], 0.5)
+  expect_equal(nodeapply(out, ids = p[[2]], info_node)[[1]], 0.5)
+  #expect_equal(nodeapply(out, ids = p[[3]], info_node)[[1]], NA)
+  expect_equal(nodeapply(out, ids = p[[4]], info_node)[[1]], 2.5)
+  expect_equal(nodeapply(out, ids = p[[5]], info_node)[[1]], 2.5)
+  #expect_equal(nodeapply(out, ids = p[[6]], info_node)[[1]], NA)
 })
 
+
 #square hinge, margin = 0, test mmit
-out<-predict(mmit(a~., target.mat, feature.mat, loss="square"))
+out <- mmit(d~., target.mat, feature.mat, loss="square")
+p <- predict(out)
 test_that("margin=0.0 yields cost 0", {
-  expect_equal(out[[1]], 0.5)
-  expect_equal(out[[2]], 0.5)
-  expect_equal(out[[3]], NA)
-  expect_equal(out[[4]], 2.5)
-  expect_equal(out[[5]], 2.5)
-  expect_equal(out[[6]], NA)
+  expect_equal(nodeapply(out, ids = p[[1]], info_node)[[1]], 0.5)
+  expect_equal(nodeapply(out, ids = p[[2]], info_node)[[1]], 0.5)
+  #expect_equal(nodeapply(out, ids = p[[3]], info_node)[[1]], NA)
+  expect_equal(nodeapply(out, ids = p[[4]], info_node)[[1]], 2.5)
+  expect_equal(nodeapply(out, ids = p[[5]], info_node)[[1]], 2.5)
+  #expect_equal(nodeapply(out, ids = p[[6]], info_node)[[1]], NA)
 })
+
