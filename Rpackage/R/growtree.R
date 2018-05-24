34cf6da40faa6d927dd36f0276dcd09e547283da
parismita <parismita@iitg.ernet.in>
2018-05-24 12:00:12 +0530

    Update growtree.R

--- Rpackage/R/growtree.R
+++ Rpackage/R/growtree.R
@@ -1,30 +1,32 @@
-growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,margin=0.0,loss="hinge",id = 1L, min_sample = 1,pred=NULL, side = NULL) {
+growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
+                               margin=0.0, loss="hinge", id = 1L, min_sample = 1,
+                               pred=NULL, side = NULL) {
 
   #if depth exceeds we stop the further growth of tree
   if (depth >= maxdepth) return(partynode(id = id, info = pred[[side]]))
   
 
   #if sample per node is less than minimum, we stop tree growth
-  if(length(target.mat[,1])<=min_sample) return(partynode(id = id, info = pred[[side]]))
+  if(length(target.mat[,1]) <= min_sample) return(partynode(id = id, info = pred[[side]]))
 
   #split the tree at the node.
   #print(length(feature.mat[,2]))
-  sp <- bestsplit(target.mat,feature.mat,margin,loss)
+  sp <- bestsplit(target.mat, feature.mat, margin, loss)
   splt <- sp
 
   #if no split, we stop
   if (is.null(sp)) return(partynode(id = id, info = pred[[side]]))
 
   #partysplit object
-  sp <- partysplit(varid = sp$varid,breaks = sp$br)
+  sp <- partysplit(varid = sp$varid, breaks = sp$br)
 
   #the kids left and right defined
-  kid<-NULL
-  kid$left<-NULL
-  kid$right<-NULL
-  tar<-NULL
-  tar$left<-NULL
-  tar$right<-NULL
+  kid <- NULL
+  kid$left <- NULL
+  kid$right <- NULL
+  tar <- NULL
+  tar$left <- NULL
+  tar$right <- NULL
 
   #splitting into kids
   kidids <- kidids_split(sp, data = feature.mat) #list of right left kids
@@ -34,17 +36,17 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
   kids <- vector(mode = "list", length = max(kidids, na.rm = TRUE))
   
   #if no left right kids, stop tree growth
-  if(length(kids)<=1) return(partynode(id = id, info = pred[[side]]))
+  if(length(kids) <= 1) return(partynode(id = id, info = pred[[side]]))
 
   #creating list of data into
   for(i in 1:length(feature.mat[,2])){
-    if(kidids[i]==1){
-      kid$left <- rbind(kid$left,feature.mat[i,])
-      tar$left <- rbind(tar$left,target.mat[i,])
+    if(kidids[i] == 1){
+      kid$left <- rbind(kid$left, feature.mat[i,])
+      tar$left <- rbind(tar$left, target.mat[i,])
     }
     else{
-      kid$right <- rbind(kid$right,feature.mat[i,])
-      tar$right <- rbind(tar$right,target.mat[i,])
+      kid$right <- rbind(kid$right, feature.mat[i,])
+      tar$right <- rbind(tar$right, target.mat[i,])
     }
   }
 
