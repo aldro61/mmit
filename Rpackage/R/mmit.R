a8db78ca603abdf28d1c78959c945150b740af43
parismita <parismita@iitg.ernet.in>
2018-05-25 16:06:37 +0530

    Update mmit.R

--- Rpackage/R/mmit.R
+++ Rpackage/R/mmit.R
@@ -21,11 +21,3 @@ mmit <- structure(function(formula, target.mat, feature.mat, weights = NULL,
 })
 
 
-#example
-#library(survival)
-#data(neuroblastomaProcessed, package="penaltyLearning")
-#survTarget <- with(neuroblastomaProcessed, {Surv(target.mat[, 1], target.mat[,2], type="interval2")})
-#feature.mat <- data.frame(log.penalty=survTarget, neuroblastomaProcessed$feature.mat)[1:45,]
-#target.mat <- neuroblastomaProcessed$target.mat[1:45,]
-#tree <- mmit(log.penalty~.,target.mat, feature.mat, maxdepth = 2,margin = 1.0)
-#plot(tree)
