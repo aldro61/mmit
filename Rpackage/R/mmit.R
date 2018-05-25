008a1c5255a704b20f412740da8a36c37f50baa3
parismita <parismita@iitg.ernet.in>
2018-05-25 18:16:35 +0530

    Update mmit.R

--- Rpackage/R/mmit.R
+++ Rpackage/R/mmit.R
@@ -2,17 +2,17 @@ mmit <- structure(function(formula, target.mat, feature.mat, weights = NULL,
                            depth=0, maxdepth = Inf, margin=0.0, loss="hinge",
                            id = 1L, min_sample = 1) {
   
-  #arranging such that response is not present in feature.mat.
+  ### arranging such that response is not present in feature.mat.
   response <- all.vars(formula)[1]
   feature.mat <- model.frame(formula, data = feature.mat)
   if(response %in% colnames(feature.mat)){
-    feature.mat$response <- NULL 
+    feature.mat[[1]] <- NULL 
   }
   
-  #tree
+  ### tree
   node<- growtree(target.mat, feature.mat, maxdepth = maxdepth, margin = margin)
   
-  ## compute terminal node number for each observation
+  ### compute terminal node number for each observation
   tree <- party(node, data = feature.mat, fitted = data.frame("(fitted)" = fitted_node(node, data = feature.mat),
                                                           "(response)" = model.response(feature.mat),
                                                           check.names = FALSE), terms = terms(feature.mat))
