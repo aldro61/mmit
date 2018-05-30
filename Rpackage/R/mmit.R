99a9dae60019162906b6085d14ca95cc6b9f1605
parismita <parismita@iitg.ernet.in>
2018-05-31 00:29:40 +0530

    Update mmit.R

--- Rpackage/R/mmit.R
+++ Rpackage/R/mmit.R
@@ -9,12 +9,17 @@ mmit <- structure(function(formula, target.mat, feature.mat, weights = NULL,
     feature.mat[[1]] <- NULL 
   }
   
+  ### assigning all weights as 1 in the beginning. weights determine which data is to be considered in which node.
+  if (is.null(weights)) weights <- rep(1L, nrow(feature.mat))
+  stopifnot(length(weights) == nrow(feature.mat) & max(abs(weights - floor(weights))) < .Machine$double.eps)
+  
   ### tree
-  node<- growtree(target.mat, feature.mat, maxdepth = maxdepth, margin = margin)
+  node<- growtree(target.mat, feature.mat, maxdepth = maxdepth, margin = margin, weights = weights)
   
   ### compute terminal node number for each observation
   tree <- party(node, data = feature.mat, fitted = data.frame("(fitted)" = fitted_node(node, data = feature.mat),
                                                           "(response)" = model.response(feature.mat),
+                                                          "(weights)" = weights,
                                                           check.names = FALSE), terms = terms(feature.mat))
 
   return(tree)
