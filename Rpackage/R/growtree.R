44ff3321093a810fc60b2a254d857172625c72b8
parismita <parismita@iitg.ernet.in>
2018-06-01 22:59:41 +0530

    Update growtree.R

--- Rpackage/R/growtree.R
+++ Rpackage/R/growtree.R
@@ -1,20 +1,27 @@
 growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
                                margin = 0.0, loss="hinge", id = 1L, min_sample = 1,
                                pred=NULL, side = NULL, weights = NULL) {
+  
+  pid <- NULL
+  ### creating dataframe of info
+  if(!is.null(pred)){
+    pid <- data.frame(pred[[side]], pred[[1]],row.names = "")
+    colnames(pid) <- c("prediction", "cost")
+  }
 
   ### if depth exceeds we stop the further growth of tree
-  if (depth >= maxdepth) return(partynode(id = id, info = pred[[side]]))
+  if (depth >= maxdepth) return(partynode(id = id, info = pid))
   
 
   ### if sample per node is less than minimum, we stop tree growth
-  if(sum(weights) <= min_sample) return(partynode(id = id, info = pred[[side]]))
+  if(sum(weights) <= min_sample) return(partynode(id = id, info = pid))
 
   ### split the tree at the node.
   sp <- bestsplit(target.mat, feature.mat, weights, margin, loss, pred)
   splt <- sp
 
   ### if no split, we stop
-  if (is.null(sp)) return(partynode(id = id, info = pred[[side]]))
+  if (is.null(sp)) return(partynode(id = id, info = pid))
 
   ### partysplit object
   sp <- partysplit(varid = sp$varid, breaks = sp$br)
@@ -26,7 +33,7 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
   kids <- vector(mode = "list", length = max(kidids, na.rm = TRUE))
   
   ### if no left right kids, stop tree growth
-  if(length(kids) <= 1) return(partynode(id = id, info = pred[[side]]))
+  if(length(kids) <= 1) return(partynode(id = id, info = pid))
 
 
   for (kidid in 1:length(kids)) {
@@ -48,7 +55,7 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
                               id = as.integer(myid + 1),min_sample = min_sample, weights = w)
   }
   
-  return(partynode(id = as.integer(id), split = sp, kids = kids, info = pred[[side]]))
+  return(partynode(id = as.integer(id), split = sp, kids = kids, info = pid))
 })
 
 
