9c46cccccbca245800903e72d76b92e61e25a663
parismita <das.parismita@gmail.com>
2018-06-05 21:03:39 +0530

    comment about size = 10, pid changed to node_info_print

--- Rpackage/R/growtree.R
+++ Rpackage/R/growtree.R
@@ -2,35 +2,36 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
                                margin = 0.0, loss="hinge", id = 1L, min_sample = 1,
                                pred=NULL, side = NULL, weights = NULL) {
   
-  pid <- NULL
+  node_info_print <- NULL
   ### creating dataframe of info
   if(!is.null(pred)){
-    pid <- data.frame(pred[[side]], pred[[1]],row.names = "")
-    colnames(pid) <- c("prediction", "cost")
+    node_info_print <- data.frame(pred[[side]], pred[[1]],row.names = "")
+    colnames(node_info_print) <- c("prediction", "cost")
   }
-
+  
+  ### passing side = 10 as arguement if node is root.
   if(side == 10){
     ### calculate root node cost (as no split thus leftcost + rightcost = 0 + cost )
-    pid <- compute_optimal_costs(target.mat, margin, loss)
-    pid <- cbind(pid[[2]], pid[[3]])
-    pid <- pid[length(pid[,1]),]
-    pid <- data.frame(pid[1], pid[2], row.names = "")
-    colnames(pid) <- c("prediction", "cost")
+    node_info_print <- compute_optimal_costs(target.mat, margin, loss)
+    node_info_print <- cbind(node_info_print[[2]], node_info_print[[3]])
+    node_info_print <- node_info_print[length(node_info_print[,1]),]
+    node_info_print <- data.frame(node_info_print[1], node_info_print[2], row.names = "")
+    colnames(node_info_print) <- c("prediction", "cost")
   }
   
   ### if depth exceeds we stop the further growth of tree
-  if (depth >= maxdepth) return(partynode(id = id, info = pid))
+  if (depth >= maxdepth) return(partynode(id = id, info = node_info_print))
   
 
   ### if sample per node is less than minimum, we stop tree growth
-  if(sum(weights) <= min_sample) return(partynode(id = id, info = pid))
+  if(sum(weights) <= min_sample) return(partynode(id = id, info = node_info_print))
 
   ### split the tree at the node.
   sp <- bestsplit(target.mat, feature.mat, weights, margin, loss, pred)
   splt <- sp
 
   ### if no split, we stop
-  if (is.null(sp)) return(partynode(id = id, info = pid))
+  if (is.null(sp)) return(partynode(id = id, info = node_info_print))
 
   ### partysplit object
   sp <- partysplit(varid = sp$varid, breaks = sp$br)
@@ -42,7 +43,7 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
   kids <- vector(mode = "list", length = max(kidids, na.rm = TRUE))
   
   ### if no left right kids, stop tree growth
-  if(length(kids) <= 1) return(partynode(id = id, info = pid))
+  if(length(kids) <= 1) return(partynode(id = id, info = node_info_print))
 
 
   for (kidid in 1:length(kids)) {
@@ -64,7 +65,7 @@ growtree <- structure(function(target.mat, feature.mat, depth=0, maxdepth = Inf,
                               id = as.integer(myid + 1),min_sample = min_sample, weights = w)
   }
   
-  return(partynode(id = as.integer(id), split = sp, kids = kids, info = pid))
+  return(partynode(id = as.integer(id), split = sp, kids = kids, info = node_info_print))
 })
 
 
