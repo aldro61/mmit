642933b66246f067c11d1c0dd327af5b4c2ecdfb
parismita <das.parismita@gmail.com>
2018-06-05 22:50:00 +0530

    fix bugs in weakest link

--- Rpackage/R/weakestLink.R
+++ Rpackage/R/weakestLink.R
@@ -1,89 +1,88 @@
 weakest_link <- structure(function(tree){
-  tree <- nodeapply(tree, ids=1)[[1]]
+  tree <- nodeapply(tree, ids = 1)[[1]]
   
-  find_weakest_link <- function(tree,soln = NULL){
-    #print(soln)
-    #get node id and cost of the current node
+  find_weakest_link <- function(tree){
+    ### get node id and cost of the current node
     ter_id <- nodeids(tree, terminal = TRUE)
     all_id <- nodeids(tree, terminal = FALSE)
-    #print(all_id[1])
-    node_cost <- as.numeric(nodeapply(tree, ids=all_id[1],info_node)[[1]][2])
+    node_cost <- as.numeric(nodeapply(tree, ids = all_id[1], info_node)[[1]][2])
     node_id <- all_id[1]
-    #print(c(length(ter_id),length(all_id)))
-    #catch numeric zero of the root
+    
+    ### catch numeric zero of the root
     if(length(node_cost) == 0){
       node_cost <- Inf
     }
     
+    ### leaf reached
     if(is.terminal(tree)){
-      #print("ture")
-      return(rbind(soln,c(Inf,node_id)))
-      
+      return(rbind(c(Inf, node_id)))
     }
     else{
       
-      #terminal leaf info
-      n <- nodeapply(tree, ids=ter_id,info_node)
+      ### terminal leaf info
+      n <- nodeapply(tree, ids = ter_id, info_node)
       
-      #sum of cost of tree
-      C_Tt <- sum(matrix(unlist(n),nrow = length(n), byrow = T)[,2])
+      ### sum of cost of tree
+      C_Tt <- sum(matrix(unlist(n), nrow = length(n), byrow = T)[,2])
       
-      #current alpha value
-      current_gt <- (node_cost - C_Tt)/(length(ter_id)-1)
+      ### current alpha value
+      current_gt <- (node_cost - C_Tt)/(length(ter_id) - 1)
       
       ### left right child ids and partynodes
       left_kid_id <- sapply(kids_node(tree), id_node)[1]
       right_kid_id <- sapply(kids_node(tree), id_node)[2]
   
-      soln <- find_weakest_link(nodeapply(tree, ids=left_kid_id)[[1]],soln)
-
+      ### weakest link from left tree part
+      soln <- find_weakest_link(nodeapply(tree, ids = left_kid_id)[[1]])
       left_min_gt <- tail(soln)[1]
       left_weakest_links <- tail(soln)[2]
       
-      
-      soln <- find_weakest_link(nodeapply(tree, ids=right_kid_id)[[1]],soln)
+      ### weakest link from right tree part
+      soln <- find_weakest_link(nodeapply(tree, ids = right_kid_id)[[1]])
       right_min_gt <- tail(soln)[1]
       right_weakest_links <- tail(soln)[2]
       
       if(isTRUE(all.equal(current_gt, min(left_min_gt, right_min_gt)))){
+        ### if all gt same
         if(isTRUE(all.equal(left_min_gt, right_min_gt))){
-          soln <- rbind(soln,c(current_gt, node_id),c(current_gt,left_weakest_links), c(current_gt,right_weakest_links))
+          soln <- rbind(c(current_gt, node_id), c(current_gt, left_weakest_links), c(current_gt, right_weakest_links))
           return(soln)
         }
         else{
+          ### if left gt lesser but equal to curentgt
           if(left_min_gt < right_min_gt){
-            soln <- rbind(soln,c(current_gt, node_id),c(current_gt,left_weakest_links))
+            soln <- rbind(c(current_gt, node_id), c(current_gt, left_weakest_links))
             return(soln)
           }
           else{
-            soln <- rbind(soln,c(current_gt, node_id), c(current_gt,right_weakest_links))
+            ### if right gt lesser but equal to curentgt
+            soln <- rbind(c(current_gt, node_id), c(current_gt, right_weakest_links))
             return(soln)
           }
         }
       }
+      ### if current gt lesser than child's
       else if(current_gt < min(left_min_gt, right_min_gt)){
-        soln <- rbind(soln,c(current_gt, node_id))
+        soln <- rbind(c(current_gt, node_id))
         return(soln)
       }
+      ### if current gt larger than child's and child's gt same
       else if(isTRUE(all.equal(left_min_gt, right_min_gt))){
-        soln <- rbind(soln,c(left_min_gt, node_id), c(left_min_gt, right_weakest_links))
+        soln <- rbind(c(left_min_gt, node_id), c(left_min_gt, right_weakest_links))
         return(soln)
       }
+      ### if current gt larger than child's and left > right
       else if(left_min_gt > right_min_gt){
-        soln <- rbind(soln, c(right_min_gt, right_weakest_links))
+        soln <- rbind(c(right_min_gt, right_weakest_links))
         return(soln)
       }
+      ### if current gt larger than child's and left < right
       else if(left_min_gt < right_min_gt){
-        soln <- rbind(soln,c(left_min_gt, node_id))
+        soln <- rbind(c(left_min_gt, left_weakest_links))
         return(soln)
       }
-      else{
-        return(soln)
-      }
-       
-    }}
+    } 
+  }
   return(find_weakest_link(tree))
 })
 
-k <- weakest_link(tree)
-View(k)
