77402744b3b4e860ce0ee648a0c08e1a186fcbad
parismita <das.parismita@gmail.com>
2018-06-06 00:20:20 +0530

    update pruning

--- Rpackage/R/pruning.R
+++ Rpackage/R/pruning.R
@@ -1,4 +1,4 @@
-secPruning <- structure(function(tree){
+pruning <- structure(function(tree){
   ### T1 is the tree after initital pruning of Tmax
   T1 <- init_pruning(tree)
   T1 <- nodeapply(T1, ids = 1)[[1]]
@@ -20,18 +20,18 @@ secPruning <- structure(function(tree){
     
     ###
     if(is.terminal(tree)){
-      return(rbind(c(mit_gt,tree)))
+      return(rbind(c(mit_gt, tree)))
     }
     else{
-      return(rbind(c(mit_gt,tree), sequential_prune(tree)))
+      return(rbind(c(mit_gt, tree), sequential_prune(tree)))
     }
   }
   
   if(is.terminal(T1)){
-    return(rbind(c(0.0,T1)))
+    return(rbind(c(0.0, T1)))
   }
   else{
-    return(rbind(c(0.0,T1), sequential_prune(T1)))
+    return(rbind(c(0.0, T1), sequential_prune(T1)))
   }
   
 })
