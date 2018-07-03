b61bf54f3e49ed372d9f5a2e8172badb9f1fd695
parismita <das.parismita@gmail.com>
2018-07-03 11:24:35 +0530

    added loss as param, in mmit.cv

--- Rpackage/R/mmit.cv.R
+++ Rpackage/R/mmit.cv.R
@@ -1,21 +1,16 @@
 mmit.cv <- structure(function(target.mat, feature.mat, 
                               param_grid, feature_names = NULL, 
-                              n_folds = 3, scorer = NULL, loss = "hinge",
+                              n_folds = 3, scorer = NULL,
                               pruning = TRUE){
   
   ### combinations of all parameters grid
-  parameters <- NULL
-  row <- nrow(param_grid)
-  col <- ncol(param_grid)
-  parameters$maxdepth <- rep(param_grid[,1], each = row**(col-1))
-  parameters$margin <- rep(param_grid[,2], each = row**(col-2), times = row)
-  parameters$min_sample <- rep(param_grid[,3], each = row**(col-3), times = row**2)
+  parameters <- expand.grid(max_depth = param_grid$maxdepth, margin = param_grid$margin, min_sample = param_grid$min_sample, loss = param_grid$loss)
   parameters <- as.data.frame(parameters)
   
   ### for param grid run loop to check best value
   cv_result <- NULL
   best_result <- NULL
-  best_result$best_score <- -Inf
+  best_result$best_score <- attr(scorer, "worst")
   for(i in 1:nrow(parameters)){
       ### see all permutation combination of param grid values
       cv_result[[i]] <- fit_and_score(target.mat = target.mat, feature.mat = feature.mat, 
@@ -23,7 +18,7 @@ mmit.cv <- structure(function(target.mat, feature.mat,
                                        n_folds = n_folds, scorer = scorer, loss = loss,
                                        pruning = pruning)
 
-      if(best_result$best_score >= cv_result[[i]]$best_score){
+      if(attr(scorer, "direction")(best_result$best_score, cv_result[[i]]$best_score) == cv_result[[i]]$best_score){
         best_result <- cv_result[[i]]
       }
   }
@@ -37,11 +32,13 @@ mmit.cv <- structure(function(target.mat, feature.mat,
   feature.mat <- data.frame(neuroblastomaProcessed$feature.mat)[1:45,]
   target.mat <- neuroblastomaProcessed$target.mat[1:45,]
   
-  param_grid <- rbind(c(Inf, 2, 1), c(4, 3, 5), c(3,4,6), c(2, 5, 10))
-  colnames(param_grid) <- c("maxdepth", "margin", "min_sample")
-  param_grid <- as.data.frame(param_grid)
+  param_grid <- NULL
+  param_grid$maxdepth <- c(Inf, 4, 3)
+  param_grid$margin <- c(2, 3, 5)
+  param_grid$min_sample <- c(2, 5, 10)
+  param_grid$loss <- c("hinge")
   
-  result <- mmit.cv(target.mat, feature.mat, param_grid)
+  result <- mmit.cv(target.mat, feature.mat, param_grid, scorer = mse)
   
   
 })
