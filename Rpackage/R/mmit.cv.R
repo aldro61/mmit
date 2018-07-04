9b7ba70670a89362ae744c40f5cec74f668d737a
parismita <das.parismita@gmail.com>
2018-07-04 21:00:55 +0530

    parallalize the for loop mmit.cv

--- Rpackage/R/mmit.cv.R
+++ Rpackage/R/mmit.cv.R
@@ -11,18 +11,25 @@ mmit.cv <- structure(function(target.mat, feature.mat,
   cv_result <- NULL
   best_result <- NULL
   best_result$best_score <- attr(scorer, "worst")
-  for(i in 1:nrow(parameters)){
-      ### see all permutation combination of param grid values
-      cv_result[[i]] <- fit_and_score(target.mat = target.mat, feature.mat = feature.mat, 
-                                       parameters = parameters[i,], feature_names = feature_names, 
-                                       n_folds = n_folds, scorer = scorer,
-                                       pruning = pruning)
-
-      if(attr(scorer, "direction")(best_result$best_score, cv_result[[i]]$best_score) == cv_result[[i]]$best_score){
-        best_result <- cv_result[[i]]
-      }
-  }
   
+  ### parallelize using foreach, see all permutation combination of param grid values
+  ### register parallel backend
+  cl <- makeCluster(2)
+  registerDoParallel(cl)
+  
+  cv_result <- foreach(i = 1:nrow(parameters), 
+              .packages = c("mmit", "assertthat")) %dopar% 
+              fit_and_score(target.mat = target.mat, feature.mat = feature.mat, 
+                                           parameters = parameters[i,], feature_names = feature_names, 
+                                           n_folds = n_folds, scorer = scorer,
+                                           pruning = pruning)
+      
+  for(i in 1:nrow(parameters)){
+    if(attr(scorer, "direction")(best_result$best_score, cv_result[[i]]$best_score) == cv_result[[i]]$best_score){
+      best_result <- cv_result[[i]]
+    }
+  }    
+    
   return(best_result)
   
 }, ex=function(){
