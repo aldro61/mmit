bb6da88771fde0f768034f8be29c296d1ed57be4
parismita <das.parismita@gmail.com>
2018-07-20 13:54:14 +0530

    create mmif.predict

--- Rpackage/R/mmif.R
+++ Rpackage/R/mmif.R
@@ -5,12 +5,11 @@
 #' @param target.mat The response variable of the model
 #' @param feature.mat a data frame containing the feature variables in the model.
 #' @param parameters the list of paramaters such as max_depth, margin, loss, min_sample
-#' @param test_feature.mat The testing data table.
 #' @param n_trees The Number of trees
 #' @param n_example  The number of data elements to be sampled from dataset.
 #' @param n_features The number of features to be sampled.
 #' 
-#' @return predicted values of random forest
+#' @return List of ensemble of trees.
 #' 
 #' @author Toby Dylan Hocking, Alexandre Drouin, Torsten Hothorn, Parismita Das
 #' 
@@ -26,15 +25,12 @@
 #' 
 #' colnames(feature.mat) <- c("a", "b", "c")
 #' feature.mat <- data.frame(feature.mat)
-#' 
-#' testx <- data.frame(neuroblastomaProcessed$feature.mat)[45:90,]
-#' testy <- neuroblastomaProcessed$target.mat[45:90,]
 #' parameters <- list(max_depth = Inf, margin = 2.0, loss = "hinge", min_sample = 1)
 #' 
-#' pred <- mmif(target.mat, feature.mat, test, parameters = parameters)
+#' trees <- mmif(target.mat, feature.mat, test, parameters = parameters)
 #' 
 #' @export
-mmif <- structure(function(target.mat, feature.mat, test_feature.mat = feature.mat,
+mmif <- structure(function(target.mat, feature.mat, 
                                    parameters, n_trees = 10,
                                    n_example = 100, 
                                    n_features = as.integer(ncol(feature.mat)**0.5)){
@@ -63,27 +59,17 @@ mmif <- structure(function(target.mat, feature.mat, test_feature.mat = feature.m
     tree <- mmit(new_target.mat, new_feature.mat, margin = parameters$margin, loss = parameters$loss, 
                  min_sample = parameters$min_sample, max_depth = parameters$max_depth)
     all_trees[[i]] <- tree
-    
-    prediction <- mmit.predict(tree, test_feature.mat)
-    all_pred <- rbind(all_pred, prediction)
   }
   
-  return(colMeans(all_pred))
+  return(all_trees)
   
 }, ex=function(){
   
   data(neuroblastomaProcessed, package="penaltyLearning")
   feature.mat <- data.frame(neuroblastomaProcessed$feature.mat)[1:45,]
   target.mat <- neuroblastomaProcessed$target.mat[1:45,]
-  testx <- data.frame(neuroblastomaProcessed$feature.mat)[45:90,]
-  testy <- neuroblastomaProcessed$target.mat[45:90,]
   parameters <- list(max_depth = Inf, margin = 2.0, loss = "hinge", min_sample = 1)
-  pred <- mmif(target.mat, feature.mat, testx, parameters = parameters)
-  
-  tree <- mmit(target.mat, feature.mat)
-  fit <- mmit.predict(tree, testx)
-  
-  score1 <-mse(testy, pred)
-  score2 <- mse(testy, fit)
+  trees <- mmif(target.mat, feature.mat, parameters = parameters)
+
 
 })
---
+++ Rpackage/R/mmif.predict.R
@@ -0,0 +1,43 @@
+#' Prediction for Random Forest
+#'
+#' Predictions as average of all predictions of the random forest.
+#' 
+#' @param trees Ensembe of trees
+#' @param test_feature.mat a data frame containing the test feature variables in the model.
+#' 
+#' @return Prediction values
+#' 
+#' @author Toby Dylan Hocking, Alexandre Drouin, Torsten Hothorn, Parismita Das
+#' 
+#' @examples
+#' library(mmit)
+#' target.mat <- rbind(
+#'   c(0,1), c(0,1), c(0,1),
+#'   c(2,3), c(2,3), c(2,3))
+#' 
+#' feature.mat <- rbind(
+#'   c(1,0,0), c(1,1,0), c(1,2,0),
+#'   c(1,3,0), c(1,4,0), c(1,5,0))
+#' 
+#' colnames(feature.mat) <- c("a", "b", "c")
+#' feature.mat <- data.frame(feature.mat)
+#' 
+#' testx <- data.frame(neuroblastomaProcessed$feature.mat)[45:90,]
+#' testy <- neuroblastomaProcessed$target.mat[45:90,]
+#' parameters <- list(max_depth = Inf, margin = 2.0, loss = "hinge", min_sample = 1)
+#' 
+#' trees <- mmif(target.mat, feature.mat, test, parameters = parameters)
+#' pred <- mmif.predict(trees, testx)
+#' 
+#' @export
+mmif <- structure(function(trees, test_feature.mat = NULL){
+  
+  all_pred <- NULL
+  for(i in 1 : length(trees)){
+    prediction <- mmit.predict(trees[[i]], test_feature.mat)
+    all_pred <- rbind(all_pred, prediction)
+  }
+  
+  return(colMeans(all_pred))
+  
+})
